2a56d4a04f26cbe53f97cda22c06d70a736eccb0
vptcoder <vptcoder@outlook.com>
2024-05-10 00:39:08 +0800

    update test for add() #1

--- tests/engine-add.test.ts
+++ tests/engine-add.test.ts
@@ -6,30 +6,58 @@ test('engine add - save successful', () => {
     val: number
     check: number
   }
-  const engine = new SimpleStateEngine('machine-id').add({
-    fromState: 'new',
-    action: 'save',
-    validation: ({ val, check }: validationNewSave) => {
-      console.log('val: ', val, 'check: ', check)
-      console.log('val is bigger than check: ', val >= check)
-      return val >= check
-    },
-    onValidationFailed: () => {
-      console.log('ON VALIDATION FAILED: pretends alert user...')
-    },
-    toState: 'draft',
-    sideEffect: () => {
-      console.log('SIDE EFFECT: state has changed')
-    },
-    onSideEffectError: () => {}
-  })
+  type validationDraftSubmit = validationNewSave
+
+  const engine = new SimpleStateEngine('machine-id')
+    .add({
+      fromState: 'new',
+      action: 'save',
+      validation: ({ val, check }: validationNewSave) => {
+        console.log('val: ', val, 'check: ', check)
+        console.log('val is bigger than check: ', val >= check)
+        return val >= check
+      },
+      onValidationFailed: () => {
+        console.log('ON VALIDATION FAILED: pretends alert user...')
+      },
+      toState: 'draft',
+      sideEffect: () => {
+        console.log('SIDE EFFECT: saved successfully')
+      },
+      onSideEffectError: () => {}
+    })
+    .add({
+      fromState: 'draft',
+      action: 'submit',
+      validation: ({ val, check }: validationNewSave) => {
+        console.log('val: ', val, 'check: ', check)
+        console.log('val is bigger than check: ', val >= check)
+        return val >= check
+      },
+      onValidationFailed: () => {
+        console.log('ON VALIDATION FAILED: pretends alert user...')
+      },
+      toState: 'submitted',
+      sideEffect: () => {
+        console.log('SIDE EFFECT: submitted successfully')
+      },
+      onSideEffectError: () => {}
+    })
 
   const current = 'new'
-  const newState = engine
-    .validate(current, 'save', { val: 4.25, check: 3 } as validationNewSave)
+  const saved = engine
+    .validate(current, 'save', { val: 4.25, check: 3 } as validationDraftSubmit)
     .progress(current, 'save')
 
-  console.log('NEW STATE: ', newState)
+  console.log('NEW STATE: ', saved)
+  expect(saved).toBe('draft')
 
-  expect(newState).toBe('draft')
+  const submitted = engine
+    .validate(saved, 'submit', {
+      val: 4.25,
+      check: 3
+    } as validationNewSave)
+    .progress(saved, 'submit')
+  console.log('NEW STATE: ', submitted)
+  expect(submitted).toBe('submitted')
 })
